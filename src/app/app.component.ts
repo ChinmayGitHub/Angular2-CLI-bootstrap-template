1ad9bc1af37beb0b0ab197825a590cde868e7f9e
Sonu Sathyadas <sonusathyadas@hotmail.com>
2017-03-10 00:49:59 +0530

    Angular2 CLI seed project using webpack with bootstrap design
    Angular2 CLI seed project using webpack with bootstrap design

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'views/app.html',
+  styleUrls: ['styles/app.css']
+})
+export class AppComponent {
+
+}
