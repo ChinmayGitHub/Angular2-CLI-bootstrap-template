2d93e78d68c4a1d6a96bb0e45146482fa61b0a07
Sonu Sathyadas <sonusathyadas@hotmail.com>
2017-06-02 12:11:41 +0530

    Updated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,17 @@
-
-import {Component} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "app/services/data.service";
 
 @Component({
-    selector:'home',
-    templateUrl:'home.html'
+	selector: 'home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 
 }
